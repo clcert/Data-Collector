fd55ec689a33d5b2f43285bb015ca2d03966ae63
Eduardo Acha <eduardoacha@gmail.com>
2015-12-08 19:49:42 -0300

    Changed Normalize http to ignore header with length equal to 0

--- Clean/NormalizeHttp.py
+++ Clean/NormalizeHttp.py
@@ -1,8 +1,8 @@
 def normalize_http(data):
     new_data = dict()
-    if 'ip' in  data:
+    if 'ip' in data:
         new_data['ip'] = data['ip']
-    if 'header' in data:
+    if 'header' in data and len(data['header']) != 0:
         new_data['header'] = data['header']
     if 'index' in data:
         new_data['index'] = data['index']
