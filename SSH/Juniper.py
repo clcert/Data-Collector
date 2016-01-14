10ae22568022f9ee2fbf37e92f47c67a637c3a0c
Eduardo Acha <eduardoacha@gmail.com>
2016-01-14 11:28:00 -0300

    Add juniper parser to main script

--- SSH/Juniper.py
+++ SSH/Juniper.py
@@ -1,19 +1,20 @@
-from SSH.SshProcess import SshProcess
-import Data.Metadata
 
 
-class Juniper(SshProcess):
+class Juniper:
+
+    def __init__(self):
+        pass
 
     search = "ssh-2.0-netscreen"
 
-    def test_juniper(self, data):
+    def test(self, data):
         """
         Test if the ssh server has the juniper backdoor
         :param data: SSH information
         :return:
         """
         if 'response' not in data:
-            raise Exception()
+            return False
 
         if data['response'].lower().find(self.search) != -1:
             return True
